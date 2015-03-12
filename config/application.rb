20561fe71f1a40f5322a0a8d0c385a45a8f783d8
manini <manini.gupta@gmail.com>
2015-03-12 11:52:52 -0400

    Trying to resolve Heroku precompilation of assets failing

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module HackdaySite
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << "#{Rails.root}/app/assets/videos"
+
+    config.assets.initialize_on_precompile = false
   end
 end
